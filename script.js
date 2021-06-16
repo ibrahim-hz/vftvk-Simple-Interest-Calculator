aa2951b40b2acf64bdc50178db2265b5c96a02a2
ibrahim <ibrahimdmc666@gmail.com>
2021-06-16 15:53:26 +0000

    updated

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById ("rate").value;
+    t = document.getElementById ("years").value;
+    si = parseInt((p*r*t)/100 );
+    amount = p*Math.pow((1 +r/100),t );
+    ci = amount-p;
+    document.getElementById ('num1').innerHTML ="If you deposit "+p;
+    document.getElementById ('num2').innerHTML ="at an interest rate of "+r;
+    document.getElementById ('num3').innerHTML ="You will recieve an amount of "+si;
+    document.getElementById ('num4').innerHTML ="in the year "+(202+t);
     
 }
-        
